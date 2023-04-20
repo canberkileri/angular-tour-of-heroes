381023897c03551b129e825673a2b786d7b33b45
Canberk İLERİ <canberkileri@gmail.com>
2023-04-21 02:06:17 +0300

    mock-heroes.ts data has been listed on heroes.component and an css file has been added for this page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,4 +13,11 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
